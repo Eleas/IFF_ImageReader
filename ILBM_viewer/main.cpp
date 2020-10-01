e0e781adf398c748ae9bcfefeb4e35da9b9ee0a7
Eleas <bjorn.paulsen@gmail.com>
2020-10-01 14:32:57 +0200

    separated image loading

--- ILBM_viewer/main.cpp
+++ ILBM_viewer/main.cpp
@@ -4,16 +4,20 @@
 // O------------------------------------------------------------------------------O
 
 #define OLC_PGE_APPLICATION
-#include <fstream>
-#include <cstdint>
+#include <memory>
 #include "FileData.h"
 #include "olcPixelGameEngine.h"
 
+using std::unique_ptr;
 
 // Override base class with your custom functionality
 class Viewer : public olc::PixelGameEngine
 {
+	unique_ptr<IFFReader::File> iff_file_;
+	shared_ptr<IFFReader::ILBM> ilbm_;
+
 public:
+	
 	Viewer()
 	{
 		// Name your application
@@ -22,51 +26,43 @@ public:
 	double cyclic = 0;
 
 public:
+	// Called once at the start, so create things here
 	bool OnUserCreate() override
 	{
 		// To do:
-		// * Recognize and parse EHB.
 		// * Recognize and parse HAM (whether CAMG chunk or not).
 		// * Break out Palette Matching step as object.
 
-		IFFReader::File fd("C:\\Users\\Björn\\source\\C++ projects\\IFF_ImageReader\\ILBM_viewer\\test files\\ehb.iff");
-		if (fd.GetType() == IFFReader::IFF_T::FORM_NOT_FOUND) {
+		iff_file_ = unique_ptr<IFFReader::File> (new IFFReader::File("C:\\Users\\Björn\\source\\C++ projects\\IFF_ImageReader\\ILBM_viewer\\test files\\ehb.iff"));
+		if (iff_file_->GetType() == IFFReader::IFF_T::FORM_NOT_FOUND) {
 			std::cout << "No valid IFF file found. Have you checked the file path?\n";
 			return true;
 		}
-		if (fd.GetType() == IFFReader::IFF_T::UNREADABLE) {
+		if (iff_file_->GetType() == IFFReader::IFF_T::UNREADABLE) {
 			std::cout << "IFF file mangled; cannot open.\n";
 			return true;
 		}
-		if (fd.GetType() == IFFReader::IFF_T::UNKNOWN_FORMAT) {
+		if (iff_file_->GetType() == IFFReader::IFF_T::UNKNOWN_FORMAT) {
 			std::cout << "This ILBM has an unsupported format.\n";
 			return true;
 		}
 
-		auto iff_image = fd.AsILBM();
+		ilbm_ = std::make_shared<IFFReader::ILBM>(*iff_file_->AsILBM());
+		SetScreenSize(ilbm_->width(), ilbm_->height());
 
-		SetScreenSize(iff_image->width(), iff_image->height());
+		return true;
+	}
 
-		//
-		//for (auto& px : *iff_image) {
-		//	Draw(px.x, px.y, olc::Pixel(px.r, px.g, px.b));
-		//}
-		//
-		// Previous should be removed, in favor of simpler structures.
 
-		for (unsigned int y = 0; y < iff_image->height(); ++y) {
-			for (unsigned int x = 0; x < iff_image->width(); ++x) {
-				auto px = iff_image->at(x, y);
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		for (unsigned int y = 0; y < ilbm_->height(); ++y) {
+			for (unsigned int x = 0; x < ilbm_->width(); ++x) {
+				auto px = ilbm_->at(x, y);
 				Draw(x, y, olc::Pixel(px.r, px.g, px.b));
 			}
 		}
 
-		// Called once at the start, so create things here
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override
-	{
 		return true;
 	}
 };
